70400f36dd07280f8d425ebd827125b2891b38e5
Abhijeet Sodlan <abhijeetsodlan7@gmail.com>
2026-04-30 16:33:57 +0530

    final change for today

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 import { Footer } from "@/components/layout/Footer";
 import { Navbar } from "@/components/layout/Navbar";
-import { BeforeAfterSlider } from "@/components/sections/BeforeAfterSlider";
 import { CarbonSourcesSinks } from "@/components/sections/CarbonSourcesSinks";
 import { CrisisStats } from "@/components/sections/CrisisStats";
 import { DeforestationMap } from "@/components/sections/DeforestationMap";
 import { EarthTimeMachine } from "@/components/sections/EarthTimeMachine";
-import { ForestsSection } from "@/components/sections/ForestsSection";
 import { FutureGenerationsSection } from "@/components/sections/FutureGenerationsSection";
 import { HeroSection } from "@/components/sections/HeroSection";
 import { LiveCountersSection } from "@/components/sections/LiveCountersSection";
-import { MythsVsFacts } from "@/components/sections/MythsVsFacts";
 import { QuoteStrip } from "@/components/sections/QuoteStrip";
 import { SpeciesCounter } from "@/components/sections/SpeciesCounter";
 import { TemperatureChart } from "@/components/sections/TemperatureChart";
@@ -29,11 +26,8 @@ export default function HomePage() {
         <CarbonSourcesSinks />
         <TemperatureChart />
         <CrisisStats />
-        <ForestsSection />
-        <BeforeAfterSlider />
         <SpeciesCounter />
         <FutureGenerationsSection />
-        <MythsVsFacts />
         <DeforestationMap />
         <ActionSteps />
       </main>
